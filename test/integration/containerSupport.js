d5b89f537712550c4e401c6171fbb1f4db682742
Michael Irwin <mikesir87@gmail.com>
2026-04-13 17:20:24 -0400

    Update versions used in testcontainers

--- test/integration/containerSupport.js
+++ test/integration/containerSupport.js
@@ -4,7 +4,7 @@ const { LocalstackContainer } = require("@testcontainers/localstack");
 const path = require("path");
 
 async function createAndBootstrapPostgresContainer() {
-  const postgresContainer = await new PostgreSqlContainer("postgres:17.4")
+  const postgresContainer = await new PostgreSqlContainer("postgres:17.9")
     .withCopyFilesToContainer([
       {
         source: path.join(__dirname, "../../dev/db/1-create-schema.sql"),
@@ -41,7 +41,7 @@ async function createAndBootstrapKafkaContainer() {
 
 async function createAndBootstrapLocalstackContainer() {
   const localstackContainer = await new LocalstackContainer(
-    "localstack/localstack:4.3.0",
+    "localstack/localstack:4.14.0",
   ).start();
 
   process.env.AWS_ENDPOINT_URL = localstackContainer.getConnectionUri();
